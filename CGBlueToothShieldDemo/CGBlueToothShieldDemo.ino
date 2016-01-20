67106c1b5e15bd4edc9e007e56aa693a0c546f0a
mithileshd27 <mithilesh.d27@gmail.com>
2016-01-20 18:19:40 +0530

    Fourth Iteration

--- CGBlueToothShieldDemo/CGBlueToothShieldDemo.ino
+++ CGBlueToothShieldDemo/CGBlueToothShieldDemo.ino
@@ -1,21 +1,21 @@
 #include <CGShield.h>
 #include <Wire.h>         // Require for I2C communication
 
-CGShield CS;  
+CGShield CS;
 
-#define LEDFRAME 0X41 
+#define LEDFRAME 0X41
 
-#define BUZZERFRAME 0X42 
+#define BUZZERFRAME 0X42
 
-#define BUTTONFRAME 0X43 
+#define BUTTONFRAME 0X43
 
-#define SENSORFRAME 0X45 
+#define SENSORFRAME 0X45
 
 
 
-#define RFRAME 0X47 
-#define GFRAME 0X48 
-#define BFRAME 0X49 
+#define RFRAME 0X47
+#define GFRAME 0X48
+#define BFRAME 0X49
 
 #define ACKFRAME 0X59 //Y
 #define SYNCFRAME 0X5A //Z
@@ -25,7 +25,7 @@ CGShield CS;
 boolean packetStarted=false;
 boolean packetComplete=false;
 uint8_t redIntensity=0, greenIntensity=0,blueIntensity=0;
-
+boolean buttonL, buttonR;
 
 void setup() {
 
@@ -40,11 +40,11 @@ void setup() {
 
 void loop()
 {
- 
+
   serialEvent();
   //Serial.println(round(light()*100/1024),DEC);
   //delay(100);
- 
+
 
 }
 
@@ -55,11 +55,11 @@ void serialEvent()
    if(Serial.available()> FRAMELENGTH-1)
     {
       packetComplete=false;
-      uint8_t serialStream[FRAMELENGTH]={};  
+      uint8_t serialStream[FRAMELENGTH]={};
       for(uint8_t dataCounter=0;dataCounter<FRAMELENGTH;dataCounter++)
       {
         serialStream[dataCounter]=(uint8_t)Serial.read();
-      }  
+      }
 
       if(serialStream[0]==0xFF && serialStream[FRAMELENGTH-1]==0xFF)
      {
@@ -86,13 +86,13 @@ void serialEvent()
              }
 
                  if(requestType==BUZZERFRAME)
-             { 
+             {
                   //Serial.println("BuzzerFrame Detected");
                   //uint8_t ledNumber=serialStream[2];
                   uint8_t buzzerState=serialStream[3];
                   if(buzzerState==0x01)
                   {
-                   //TODO 
+                   //TODO
                    //LED(ledNumber, HIGH);
                   }
                   else
@@ -102,13 +102,13 @@ void serialEvent()
                   }
              }
 
-              
+
                 if(requestType==RFRAME)
                 {
                   //Serial.println("Red Color Frame Detected");
                   redIntensity=serialStream[2];
                   RGB(redIntensity,greenIntensity,blueIntensity);
-                 
+
                 }
 
                 if(requestType==GFRAME)
@@ -116,7 +116,7 @@ void serialEvent()
                   //Serial.println("Green Color Frame Detected");
                   greenIntensity=serialStream[2];
                   RGB(redIntensity,greenIntensity,blueIntensity);
-                 
+
                 }
 
                 if(requestType==BFRAME)
@@ -124,42 +124,51 @@ void serialEvent()
                   //Serial.println("Blue Color Frame Detected");
                   blueIntensity=serialStream[2];
                   RGB(redIntensity,greenIntensity,blueIntensity);
-                 
-                } 
+
+                }
 
                 if(requestType==SENSORFRAME)
                 {
                   //Serial.println("Temp Color Frame Detected");
                    int lightVal=light();
                    int trimVal=trim();
+                   uint8_t buttonval = 0X0F;
+                   uint8_t right = 0XFE, left = 0XFD;
+                   buttonL = button(LEFT);
+                   buttonR = button(RIGHT);
+                   if (!buttonR){
+                    buttonval = buttonval&right;
+                   }
+                   if (!buttonL){
+                    buttonval = buttonval&left;
+                   }
                    //float lightValPercent=(lightVal/1024)*100;
                    Serial.write(0xFF);
                    Serial.write(SENSORFRAME);
-                   Serial.write(round(temp()));                   
+                   Serial.write(round(temp()));
                    Serial.write(round(floor(lightVal/256)));
                    Serial.write(round(lightVal%256));
                    Serial.write(round(floor(trimVal/256)));
                    Serial.write(round(trimVal%256));
-                   Serial.write(0x00);
+                   Serial.write(buttonval);
                    Serial.write(0xFF);
-                } 
+                }
 
                  if(requestType==BUZZERFRAME)
                 {
                     //Serial.println("Buzzer Frame Detected");
-                   
+
                     uint8_t highByteFreq=serialStream[2];
                     uint8_t lowByteFreq=serialStream[3];
                      //Serial.println(highByteFreq, DEC);
                     //Serial.println(lowByteFreq, DEC);
                     int buzzerFrequency=(highByteFreq<<8) +lowByteFreq;
                     speaker(buzzerFrequency,500);
-                } 
+                }
 
-                 
+
      }
-      
-    }
-  
-}
 
+    }
+
+}
